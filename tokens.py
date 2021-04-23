268cc6a59cfca2c64059e82b09186aaf74eb30fa
Eugene <gigantina.ru@yandex.ru>
2021-04-23 20:55:21 +0300

    исправление небольшой ошибки

--- tokens.py
+++ tokens.py
@@ -1,6 +1,6 @@
 TOKEN = "1617067709:AAEcjhyh9OtWE85kHtnLHLFCeA7mP0sqmEk"
-USERNAME = "gigandev_db"
+USERNAME = "gigandev_project"
 HOST = "gigandev.beget.tech"
 PASSWORD = "*Aw0q*vA"
-BASE_NAME = "gigandev_project"
+BASE_NAME = USERNAME
 SECRET_KEY = "a really really really really long secret key"
