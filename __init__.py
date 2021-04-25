7984a5063e6f2b79465afda79278e83f54751c61
Eugene <gigantina.ru@yandex.ru>
2021-04-25 11:31:44 +0300

    добавил тест

--- __init__.py
+++ __init__.py
@@ -1,7 +1,8 @@
 from config import app  # , login_manager
-from flask import render_template
+from flask import render_template, request
 from models import *
 from forms import *
+import requests
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -22,6 +23,30 @@ def disc():
 @app.route('/topic', methods=['GET', 'POST'])
 def topic():
     return render_template('main.html')
+
+
+@app.route('/test', methods=['GET', 'POST'])
+def test():
+    return render_template('8values/index.html')
+
+
+@app.route('/test/instructions', methods=['GET', 'POST'])
+def instructions():
+    return render_template('8values/instructions.html')
+
+
+@app.route('/test/quiz', methods=['GET', 'POST'])
+def quiz():
+    return render_template('8values/quiz.html')
+
+
+@app.route('/test/results', methods=['GET', 'POST'])
+def res():
+    e = request.args.get('e', default=1, type=float)
+    d = request.args.get('d', default=1, type=float)
+    g = request.args.get('g', default=1, type=float)
+    s = request.args.get('s', default=1, type=float)
+    return render_template('8values/results.html')
 
 
 if __name__ == '__main__':
